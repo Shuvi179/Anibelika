7c153afc4784e84980432826e81032143e724e8c
Shuvi179 <tsyapkaseveryn@gmail.com>
2021-04-21 18:56:55 +0300

    Commit 34: add cors support

--- src/main/java/com/orion/anibelika/security/ApplicationSecurityConfig.java
+++ src/main/java/com/orion/anibelika/security/ApplicationSecurityConfig.java
@@ -2,12 +2,16 @@ package com.orion.anibelika.security;
 
 import com.orion.anibelika.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -40,11 +44,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
                 .authorizeRequests()
                 .antMatchers("/css/*", "/js/*").permitAll()
                 //.antMatchers(servletPath + "/book/*").authenticated()
                 .anyRequest().permitAll()
                 .and().formLogin().failureHandler(new CustomAuthenticationFailureHandler())
                 .and().oauth2Login().defaultSuccessUrl(redirectUrl);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
